7735c15874576ea97c3b75804981266e5cf7b1de
Amanda Arruda <amandaoarruda@gmail.com>
2020-10-09 17:02:58 -0300

    no message

---
+++ js/main.js
@@ -0,0 +1,78 @@
+function validaIdade (idade) {
+    
+}
+
+
+
+/* function soma (n1, n2) {
+return n1 + n2;
+
+}
+
+function setReplace (frase, nome, novo_nome) {
+
+return frase.replace(nome, novo_nome)
+}
+
+alert (soma(5,10));
+alert (setReplace("Vai, Japão", "Japão", "Brasil") )
+*/
+
+/*
+var d = new Date ();
+alert (d.getDay());
+*/
+
+
+/*
+var count
+
+for (count=0; count<=5; count++) {
+    alert (count);
+    console.log (count);
+};
+
+*/
+
+/*
+var count = 0
+
+while (count <= 5) {
+
+    console.log(count);
+    count++
+};
+
+*/
+
+/* var idade = prompt ("Qual a sua idade?");
+
+if (idade >=18){
+alert ("Maior de Idade!");
+
+}else {
+    alert("Menor de Idade!");
+};  
+
+
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert (frutas[1].nome);
+
+
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome); */
+
+
+
+//var nome = "Amanda Arruda";
+//var idade= 30;
+//var idade2= 10;
+//var lista = ["maçã", "pera", "laranja"]
+//lista.push("uva")
+
+//alert (nome + " tem " + idade + " anos.");
+
+//alert (idade + idade2)
+//console.log(lista.toString());
+//console.log (lista.join (" - "))
